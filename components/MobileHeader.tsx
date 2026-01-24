87b530e1f5b288aa80165268c994ac2d891bff2c
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2026-01-24 15:43:50 -0600

    align mobile logo with nav button spacing

--- components/MobileHeader.tsx
+++ components/MobileHeader.tsx
@@ -19,7 +19,7 @@ export function MobileHeader() {
   }, [])
 
   return (
-    <div className="flex items-center justify-center flex-1 lg:hidden">
+    <div className="flex items-center flex-1 lg:hidden pl-14">
       <AnimatePresence>
         {!scrolled && (
           <motion.div
